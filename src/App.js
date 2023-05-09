901df04c3ad15111d2d0f910a95b1c503e3facd5
shruti <shruti.malik.ramaswamy@gmail.com>
2023-05-09 10:09:16 -0500

    cheanges

--- src/App.js
+++ src/App.js
@@ -2,68 +2,83 @@ import React, { useState } from "react";
 import styled from "styled-components";
 
 const App = () => {
-const [todos, setTodos] = useState([
-{
-text: "Buy milk",
-completed: false,
-},
-{
-text: "Wash the car",
-completed: false,
-},
-{
-text: "Take out the trash",
-completed: false,
-},
-]);
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Buy milk",
+      completed: false,
+    },
+    {
+      id: 2,
+      text: "Wash the car",
+      completed: false,
+    },
+    {
+      id: 3,
+      text: "Do the laundry",
+      completed: false,
+    },
+  ]);
 
-const handleAddTodo = (text) => {
-  setTodos([...todos, { text, completed: false }]);
-};
-
-const handleMarkTodoCompleted = (id) => {
-  setTodos(todos.map((todo) => {
-    if (todo.id === id) {
-      todo.completed = true;
+  const handleAddTodo = (text) => {
+    if (text.trim() === "") {
+      throw new Error("Error adding todo: empty text");
     }
-    return todo;
-  }));
-};
+    setTodos([
+      ...todos,
+      {
+        id: Math.random().toString(36).substring(7),
+        text,
+        completed: false,
+      },
+    ]);
+  };
 
-const handleDeleteTodo = (id) => {
-  setTodos(todos.filter((todo) => todo.id !== id));
-};
+  const handleMarkTodoCompleted = (id) => {
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = true;
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+  };
 
-return (
-  <div>
-    <h1>Todo List</h1>
-    <form onSubmit={handleAddTodo}>
-      <input type="text" placeholder="Add a todo" />
-      <button type="submit">Add</button>
-    </form>
-    <ul>
-      {todos.map((todo) => (
-        <li key={todo.id}>
-          {todo.text}
-          <button onClick={() => handleMarkTodoCompleted(todo.id)}>
-            {todo.completed ? "Uncheck" : "Check"}
-          </button>
-          <button onClick={() => handleDeleteTodo(todo.id)}>
-            Delete
-          </button>
-        </li>
-      ))}
-    </ul>
-    <h2>Completed Todos</h2>
-    <ul>
-      {todos.filter((todo) => todo.completed).map((todo) => (
-        <li key={todo.id}>
-          {todo.text}
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+  const handleDeleteTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <form onSubmit={handleAddTodo}>
+        <input type="text" placeholder="Add a todo" />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+            <button onClick={() => handleMarkTodoCompleted(todo.id)}>
+              {todo.completed ? "Uncheck" : "Check"}
+            </button>
+            <button onClick={() => handleDeleteTodo(todo.id)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+      <h2>Completed Todos</h2>
+      <ul>
+        {todos.filter((todo) => todo.completed).map((todo) => (
+          <li key={todo.id}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
